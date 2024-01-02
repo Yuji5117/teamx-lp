901b25f07c52ba958d08f7c35d17bd39e3176722
Yuji Nakamura <ny5117@icloud.com>
2024-01-02 17:09:22 +0900

    スライドショーを実装

---
+++ js/slide.js
@@ -0,0 +1,22 @@
+const swiper = new Swiper(".swiper", {
+  slidesPerView: 2,
+  spaceBetween: 40,
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 3,
+    },
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
